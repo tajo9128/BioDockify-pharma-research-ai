83b0d0e12bdf1bbe37cc1a5a80f109fd7927606c
tajo9128 <tajo9128@gmail.com>
2026-02-07 11:50:18 +0530

    chore: cleanup repository - remove unused docs, scripts, and legacy files

--- fix_icon.ps1
+++
@@ -1,9 +0,0 @@
-
-Add-Type -AssemblyName System.Drawing
-$source = "$PSScriptRoot\desktop\tauri\src-tauri\icons\icon.png"
-$dest = "$PSScriptRoot\desktop\tauri\src-tauri\icons\icon_fixed.png"
-Write-Host "Reading from: $source"
-$img = [System.Drawing.Image]::FromFile($source)
-$img.Save($dest, [System.Drawing.Imaging.ImageFormat]::Png)
-$img.Dispose()
-Write-Host "Converted to PNG at: $dest"
