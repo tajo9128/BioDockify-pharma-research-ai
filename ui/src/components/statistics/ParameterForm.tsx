4dc200ff127bbd06f3761e8c41502858bfa67900
tajo9128 <tajo9128@gmail.com>
2026-02-14 12:56:21 +0000

    fix: ParameterDefinition -> AnalysisParameter type correction v2.8.0

--- ui/src/components/statistics/ParameterForm.tsx
+++ ui/src/components/statistics/ParameterForm.tsx
@@ -134,8 +134,8 @@ export default function ParameterForm({
           >
             <option value="">{isRequired ? 'Select an option *' : 'Select an option (optional)'}</option>
             {param.options?.map((opt) => (
-              <option key={opt} value={opt}>
-                {opt}
+              <option key={opt.value} value={opt.value}>
+                {opt.label}
               </option>
             ))}
           </select>
